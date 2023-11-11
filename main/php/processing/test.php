b533d0f60f6f2e99822728b4fe3d54c0764158c7
Filip Manev <7605817-FilipManev@users.noreply.gitlab.com>
2023-11-11 17:08:59 +0100

    brainstorm 2: encryption tryouts

--- main/php/processing/test.php
+++ main/php/processing/test.php
@@ -44,15 +44,35 @@ function getChristmassDays($arr){
     return $arr;
 }
 
-$year = 2023;
+// $year = 2023;
 
-$arr = get30PrimeYears($year);
-$arr = getChristmassDays($arr);
+// $arr = get30PrimeYears($year);
+// $arr = getChristmassDays($arr);
 
-arrDebug($arr);
+// arrDebug(openssl_get_cipher_methods());
 
+$key = openssl_random_pseudo_bytes(5);
+$plaintext = "message to be encrypted";
+$cipher = "aes-128-gcm";
 
+$ivlen = openssl_cipher_iv_length($cipher);
+$iv = openssl_random_pseudo_bytes($ivlen);
 
+$encrypted = openssl_encrypt($plaintext, $cipher, $key, $options=0, $iv, $tag);
+echo $encrypted;
+$decrypted = openssl_decrypt($encrypted, $cipher, $key, $options=0, $iv, $tag);
+echo '<br>', $decrypted;
+// echo $key, '<br>', $plaintext, '<br>', $cipher, '<br>';
+// // die();
+// if (in_array($cipher, openssl_get_cipher_methods()))
+// {
+//     $ivlen = openssl_cipher_iv_length($cipher);
+//     $iv = openssl_random_pseudo_bytes($ivlen);
+//     $ciphertext = openssl_encrypt($plaintext, $cipher, $key, $options=0, $iv, $tag);
+//     //store $cipher, $iv, and $tag for decryption later
+//     $original_plaintext = openssl_decrypt($ciphertext, $cipher, $key, $options=0, $iv, $tag);
+//     echo $original_plaintext."\n";
+// }
 // for ($i=1; $i < 2001; $i++) { 
 //    echo checkPrime($i) ? '<span style="color: green">'. $i .'</span><br>' : '<span style="color: red">'. $i .'</span><br>';
 // }
